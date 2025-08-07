04370fc73f626f16279849a082a4cb8c777c5b09
guohongbin <hongbin.guo@mercedes-benz.com>
2025-08-07 17:22:50 +0800

    Update package dependencies: downgrade tailwindcss to version 3.4.3, add new dependencies, and remove tailwind.config.cjs file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: { extend: {} },
   darkMode: 'class',          // light/dark toggle
   plugins: [],
-};
+}
+
